6aaf42a4184a31c6074363c8e8b9befe8ec2fb8b
wwysopal <wioletta.wysopal@gmail.com>
2023-10-30 18:41:30 +0100

    Add powershell refresh docker script

---
+++ FRONTEND/CodeMelodies/refresh.bat
@@ -0,0 +1,14 @@
+:: Zatrzymuje wszystkie kontenery
+docker kill $(docker ps -aq)
+
+:: Usuwa wszystkie kontenery (dodano "-f" dla wymuszenia)
+docker rm -f $(docker ps -aq)
+
+:: Usuwa wszystkie obrazy
+docker rmi -f $(docker images -q)
+
+:: Buduje nowy obraz o nazwie "angularapp" z bieżącego katalogu
+docker build . -t angularapp
+
+:: Uruchamia kontener z obrazem "angularapp"
+docker run -p 4200:80 angularapp
