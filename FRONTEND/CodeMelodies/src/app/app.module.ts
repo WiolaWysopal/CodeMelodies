94bce3a170b3406e15f9f26a881c7182440c0872
wwysopal <wioletta.wysopal@gmail.com>
2023-10-31 17:09:26 +0100

    Add tlo component and style it

--- FRONTEND/CodeMelodies/src/app/app.module.ts
+++ FRONTEND/CodeMelodies/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PasekComponent } from './pasek/pasek.component';
+import { TloComponent } from './tlo/tlo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PasekComponent
+    PasekComponent,
+    TloComponent
   ],
   imports: [
     BrowserModule,
---
+++ FRONTEND/CodeMelodies/src/app/tlo/tlo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tlo',
+  templateUrl: './tlo.component.html',
+  styleUrls: ['./tlo.component.css']
+})
+export class TloComponent {
+
+}
