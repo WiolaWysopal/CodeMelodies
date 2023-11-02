449a8c1bc07d3b87e4ecbf2a7723caf25a599144
wwysopal <wioletta.wysopal@gmail.com>
2023-11-02 17:12:35 +0100

    Add login component

--- FRONTEND/CodeMelodies/src/app/app.module.ts
+++ FRONTEND/CodeMelodies/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PasekComponent } from './pasek/pasek.component';
 import { TloComponent } from './tlo/tlo.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PasekComponent,
-    TloComponent
+    TloComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
