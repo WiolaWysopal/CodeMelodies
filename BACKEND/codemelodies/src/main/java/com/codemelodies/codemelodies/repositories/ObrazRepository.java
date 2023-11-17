6160d409b0f09a70e756f695506a12492173da15
wwysopal <wioletta.wysopal@gmail.com>
2023-11-17 18:07:01 +0100

    Remove obraz related code

--- BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/repositories/ObrazRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codemelodies.codemelodies.repositories;
-
-import com.codemelodies.codemelodies.models.Obraz;
-//import org.springframework.data.jpa.repository.JpaRepository;
-
-//import java.util.UUID;
-//
-//public interface ObrazRepository extends JpaRepository<Obraz, UUID> {
-//}
