e268d4fe57ba84e650f51ae168a48617aa4a3f43
wwysopal <wioletta.wysopal@gmail.com>
2023-11-18 17:46:48 +0100

    Remove obraz model import

--- BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/ObrazyController.java
+++ BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/ObrazyController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.codemelodies.codemelodies.models.Obraz;
 import java.util.UUID;
 
 @RestController
