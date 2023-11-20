1557e515963314292be2a9d6ed57f3e841208b69
wwysopal <wioletta.wysopal@gmail.com>
2023-11-20 22:11:02 +0100

    Prepare logic for image storage in db

--- BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/ObrazyController.java
+++ BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/ObrazyController.java
@@ -10,32 +10,15 @@ import java.util.UUID;
 @RestController
 public class ObrazyController {
 
-    // Zakomentowane, ponieważ nie mam jeszcze repozytorium
-    // @Autowired
-    // private ObrazRepository obrazRepository;
-
     @GetMapping("/zdjecia")
     public String getZdjecie() {
         // Pobranie informacji o zdjeciach
         return "Informacje o zdjeciach";
     }
-    // Jak ogarnę bazę ze zdjęciami to poniższe ogarnia paginację
-//    public Page<Obraz> getZdjecia(Pageable pageable) {
-//        // Zakomentowane, ponieważ nie mam jeszcze repozytorium
-//        // return obrazRepository.findAll(pageable);
-//        return null; // tymczasowo zwróć null
-//    }
 
     @GetMapping("/ikona")
     public String getIkona() {
         // Pobranie informacji o Ikonie
         return "Informacje o ikonach";
     }
-
-    //Jak ogarnę bazę ze zdjęciami i ikonami, to poniższe gwarantuje UUID ikon
-//    public Obraz getIkona(UUID id) {
-//        // Zakomentowane, ponieważ nie mam jeszcze repozytorium
-//        // return obrazRepository.findById(id).orElse(null);
-//        return null; // tymczasowo zwróć null
-//    }
 }
