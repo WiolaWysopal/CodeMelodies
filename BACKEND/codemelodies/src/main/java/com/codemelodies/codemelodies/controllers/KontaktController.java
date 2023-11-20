09903bc0dbe175a7217a3441712ab850918b1db8
wwysopal <wioletta.wysopal@gmail.com>
2023-11-20 22:09:19 +0100

    Remove unused imports

--- BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/KontaktController.java
+++ BACKEND/codemelodies/src/main/java/com/codemelodies/codemelodies/controllers/KontaktController.java
@@ -5,12 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
-
-import static org.springframework.http.ResponseEntity.ok;
-
 
 @RestController
 public class KontaktController {
