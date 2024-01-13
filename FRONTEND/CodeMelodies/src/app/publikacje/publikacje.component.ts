dda72e71247233e0b900c10c36a9b61af6554b99
wwysopal <wioletta.wysopal@gmail.com>
2024-01-13 14:33:40 +0100

    Finish publikacje page

--- FRONTEND/CodeMelodies/src/app/publikacje/publikacje.component.ts
+++ FRONTEND/CodeMelodies/src/app/publikacje/publikacje.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-publikacje',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./publikacje.component.css']
 })
 export class PublikacjeComponent {
+  publications: any[] = [];
+  private apiUrl = 'http://localhost:8080/api/publikacje';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPublications();
+  }
+
+  fetchPublications() {
+    this.http.get<any[]>(this.apiUrl).subscribe(
+      data => this.publications = data,
+      err => console.error(err)
+    );
+  }
+
+  downloadFile(fileId: string) {
+    this.http.get(`http://localhost:8080/api/publikacja/${fileId}`, { responseType: 'blob' }).subscribe(
+      blob => {
+        // Handle the blob file (e.g., download or preview)
+        const url = window.URL.createObjectURL(blob);
+        window.open(url); // Or download it
+      },
+      err => console.error(err)
+    );
+  }
 }
