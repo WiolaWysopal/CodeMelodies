4f90affa19b9dfc19e1e566aad8af7d3b09d3505
wwysopal <wioletta.wysopal@gmail.com>
2024-01-13 11:49:52 +0100

    Finish home page

--- DB/generate-image-insert-sql.sh
+++ DB/generate-image-insert-sql.sh
@@ -14,11 +14,15 @@ for file in "$IMAGE_DIR"/*.png; do
     # Skip if no image files are found
     [ -e "$file" ] || continue
 
+    # Extract the ID from the filename (filename is the ID)
+    FILENAME=$(basename "$file")
+    ID="${FILENAME%.*}"
+
     # Read the binary data and format it as a hex string
     BINARY_DATA=$(xxd -p -c 1000000 "$file" | tr -d '\n')
 
     # Append the INSERT statement to the SQL script
-    echo "INSERT INTO Images (image) VALUES (decode('$BINARY_DATA', 'hex'));" >> "$SQL_SCRIPT"
+    echo "INSERT INTO Images (image_id, image) VALUES ('$ID',decode('$BINARY_DATA', 'hex'));" >> "$SQL_SCRIPT"
 done
 
 # Remove the script itself
--- FRONTEND/CodeMelodies/src/app/home/home.component.ts
+++ FRONTEND/CodeMelodies/src/app/home/home.component.ts
@@ -7,7 +7,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  fetchedData: string = ''; // Property to store the fetched data
+  biogramData: string = ''; // Property to store the fetched data
+  imageData: string = '';
 
   constructor(private http: HttpClient) { }
 
@@ -18,8 +19,19 @@ export class HomeComponent implements OnInit {
   private loadData() {
     this.http.get('http://localhost:8080/api/omnie', { responseType: 'text' })
     .subscribe(
-      data => this.fetchedData = data,
+      data => this.biogramData = data,
       error => console.error(error)
+    );
+    this.http.get('http://localhost:8080/api/zdjecie/ba5ae4f1-377d-4721-b690-a6edeba36449', { responseType: 'blob' })
+    .subscribe(
+      data => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.imageData = reader.result as string;
+        };
+        reader.readAsDataURL(data);
+    },
+    error => console.error(error)
     );
   }
 }
